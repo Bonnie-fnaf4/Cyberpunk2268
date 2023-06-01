a0e09e4b103f12031efe36ba1cca030131c0aadc
Bonnie-fnaf4 <103939286+Bonnie-fnaf4@users.noreply.github.com>
2023-06-01 15:12:43 +0600

    Поменял папку Гитхаба на Assets
    Перевёл всё в Assets т.к. из за Файлов в Library происходили баги. Буду тестить всё через Assets

---
+++ Scripts/AI/AIManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Animations;
+
+public class AIManager : MonoBehaviour
+{
+    public int hp = 100;
+    Rigidbody rigidbody;
+    NavMeshAgent navMeshAgent;
+    Animator animator;
+
+    float timerToShoot = 0;
+    public ShootAI shootAI;
+
+    bool isDead = false;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        timerToShoot += Time.deltaTime;
+        if(timerToShoot >= 2 && animator.GetBool("isChasing") && !isDead)
+        {
+            shootAI.Shoot();
+            timerToShoot = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.other.tag == "Bullet")
+        {
+            hp -= Random.Range(10, 25);
+            animator.SetBool("isChasing", true);
+            animator.SetBool("isPatrolling", false);
+        }
+        if (hp <= 0)
+        {
+            animator.enabled = false;
+            navMeshAgent.enabled = false;
+            rigidbody.isKinematic = false;
+            rigidbody.useGravity = true;
+            animator.SetBool("isChasing", false);
+            animator.SetBool("isAttacking", false);
+            isDead = true;
+            Destroy(gameObject, 30);
+        }
+    }
+}
